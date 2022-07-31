9e2129cb27428cfb21a6dfaa85ee204fb6503f48
kubgus <53797257+kubgus@users.noreply.github.com>
2022-07-31 23:25:09 +0200

    Added some summaries

--- KudoEngine/Input.cs
+++ KudoEngine/Input.cs
@@ -4,6 +4,9 @@
     {
         internal static Dictionary<Keys, bool> PressedKeys { get; set; } = new();
 
+        /// <summary>
+        /// Check if a certain key is pressed
+        /// </summary>
         public static bool IsKeyDown(Keys key)
         {
             return PressedKeys.ContainsKey(key);
